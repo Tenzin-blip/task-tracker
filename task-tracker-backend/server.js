a69bee04d9ccca8b792091c6ba5958ce43fc3ee4
Tenzin-blip <dolkertenzin9@gmail.com>
2025-07-09 08:50:36 +0545

    Created users.js for authentication

--- task-tracker-backend/server.js
+++ task-tracker-backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 app.use(express.json());
 const PORT = 5000;
+const users = require('./users.js');
+const JWT_SECRET = 'mysecretkey123';
 
 app.use(cors());
 app.use(express.json());
@@ -29,4 +33,19 @@ app.get("/api/tasks", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+app.post('/api/auth/login', (req,res) =>{
+  const {email,password} = req.body;
+  const user = users.find((u) => u.email === email);
+
+  if (!user || !bcrypt.compareSync(password, user.password)){
+    return res.status(401).json({message: 'Wrong email or password'});
+  }
+
+  const token = jwt.sign({id: user.id, role:user.role}, JWT_SECRET, {
+    expiresIn:'15m',
+  });
+
+  res.json({token,message: 'Login successful'});
 });
