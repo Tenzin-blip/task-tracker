e1d12abc4fd043ad93d204573f7412328fe34673
Tenzin-blip <dolkertenzin9@gmail.com>
2025-07-16 21:04:40 +0545

    Google sign in

--- task-tracker-backend/models/User.js
+++ task-tracker-backend/models/User.js
@@ -1,9 +1,9 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 }, { timestamps: true });
 
-export default mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
